b8376c86181d787b8737a9fb292231d368082b9a
ciberSi4 <alex.baikonur@mail.ru>
2024-09-05 00:51:46 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,19 @@
+def send_email (message : str, recipient : str, *, sender : str = "university.help@gmail.com"):
+
+    if ((sender.find("@") == -1 or recipient.find("@") == -1)
+            or not ((sender.endswith(".ru") or sender.endswith(".com") or (sender.endswith(".net"))
+            and (recipient.endswith(".ru") or recipient.endswith(".com") or recipient.endswith(".net"))))):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender  == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
